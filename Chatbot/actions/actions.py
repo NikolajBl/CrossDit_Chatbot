cf03bdbedf93405ecd296c71e732dfcf1fe5f621
Nikolaj-Bläser <nibl@itu.dk>
2020-10-26 18:05:43 +0100

    improved upon the first story. Added the first custom action

--- Chatbot/actions/actions.py
+++ Chatbot/actions/actions.py
@@ -7,21 +7,22 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionComputeManure(Action):
+   def name(self):
+      # type: () -> Text
+      return "action_compute_manure"
+
+   def run(self, dispatcher, tracker, domain):
+      # type: (CollectingDispatcher, Tracker, Dict[Text, Any]) -> List[Dict[Text, Any]]
+
+      area = tracker.get_slot('field_size')
+      location = tracker.get_slot('location')
+
+
+      return [SlotSet("manure_quantity", int(area)*10)]
