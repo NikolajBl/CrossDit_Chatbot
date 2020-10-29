442eb5c96af3ad9fdffc2257f8ec8b1dc1acb57c
Nikolaj-Bläser <nibl@itu.dk>
2020-10-29 17:58:33 +0100

    added 2nd story and started to make use of the pdf visualizer. Also brushed up the first story.

--- Chatbot/actions/actions.py
+++ Chatbot/actions/actions.py
@@ -28,3 +28,18 @@ class ActionComputeManure(Action):
       return [SlotSet("manure_quantity", int(area)*10),
                SlotSet("location", None),SlotSet("field_size", None),
                SlotSet("crop_type", None)]
+
+class ActionComputeManureProduction(Action):
+   def name(self):
+      # type: () -> Text
+      return "action_compute_manure_production"
+
+   def run(self, dispatcher, tracker, domain):
+      # type: (CollectingDispatcher, Tracker, Dict[Text, Any]) -> List[Dict[Text, Any]]
+      animal_number = tracker.get_slot('animal_number')
+      animal_type = tracker.get_slot('animal_type')
+      stable_type = tracker.get_slot('stable_type')
+
+      return [SlotSet("manure_production_estimate", float(animal_number) * 10.0),
+               SlotSet("animal_number", None),SlotSet("animal_type", None),
+               SlotSet("stable_type", None)]
